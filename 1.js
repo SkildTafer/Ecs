6d091b2fb4fb5c420c31e4e50a8390f085106c32
skild <skildtafer2017@mail.ru>
2024-05-23 09:28:42 +0300

    ecs

---
+++ 1.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+let A = [2, 31, 1, 4, 632, 16, -6, 0, 24, 931]
+let B = [
+  768, 31, 2316, 6123, 876, 60, 6, 31, 352, 254385, 2441, 5, 86, 8754, 214,
+]
+let maxa = A[0]
+let ia = 0
+let maxb = B[0]
+let ib = 0
+for (let i = 1; i < 10; i++) {
+  if (A[i] > maxa) {
+    maxa = A[i]
+    ia = i
+  }
+}
+for (let i = 1; i < 15; i++) {
+  if (B[i] > maxb) {
+    maxb = B[i]
+    ib = i
+  }
+}
+let otvet
+if (ia < ib) {
+  otvet =
+    'Максимальное число из первого массива (' +
+    maxa +
+    ') находится ближе к началу, чем из второго (' +
+    maxb +
+    ') ' +
+    ia +
+    ' - ' +
+    ib
+} else if (ia > ib) {
+  otvet =
+    'Максимальное число из второго массива (' +
+    maxb +
+    ') находится ближе к началу, чем из первого (' +
+    maxa +
+    ') ' +
+    ib +
+    ' - ' +
+    ia
+} else {
+  otvet =
+    'Максимальное число из первого массива (' +
+    maxa +
+    ') и максимальноке число из второго массива (' +
+    maxb +
+    ') находятся на равных индексах' +
+    ib
+}
+console.log(otvet)
+fs.readFile('otvet', 'utf8')
+  .then((data) => {
+    fs.writeFile('otvet', data + otvet)
+  })
+  .catch((err) => {
+    console.log('error')
+  })
